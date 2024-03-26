9a5bd7b6b5a72b4ad0b5ed54dc7ab71b79e016b2
Tom Cat <maoy896@gmail.com>
2024-03-26 15:47:34 +0800

    add recipe and order into Merchant

--- class.h
+++ class.h
@@ -2,17 +2,40 @@
 #define CLASSIFICATION_H
 
 // 商家结构体
-typedef struct {
+typedef struct 
+{
     char name[100];
     char address[100];
     char phone[20];
+
+    typedef struct recipe
+    {
+        char name[100];
+        int price;
+        struct recipe *next;
+    } Recipe;
+
+    typedef struct order
+    {
+        char name[100];
+        int price;
+        struct order *next;
+    } Order;
+
+    struct Merchant *next;
+
 } Merchant;
 
 // 外卖员结构体
-typedef struct {
+typedef struct 
+{
+    int state;
     char name[100];
     int age;
     char phone[20];
+
+    struct DeliveryPerson *next;
+    
 } DeliveryPerson;
 
 // 用户结构体
@@ -20,6 +43,8 @@ typedef struct {
     char name[100];
     char address[100];
     char phone[20];
+
+    struct User *next;
 } User;
 
 #endif
