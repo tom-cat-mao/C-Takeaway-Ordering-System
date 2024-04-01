ab2a3e926ded77ed8eb38002fe798e6fcdf34be5
stephenzhang0529 <143581044+stephenzhang0529@users.noreply.github.com>
2024-04-01 20:02:35 +0800

    improve
    change the class.h in this branch to the right one in the main branch

--- class.h
+++ class.h
@@ -1,32 +1,48 @@
+#pragma once
 #ifndef CLASSIFICATION_H
 #define CLASSIFICATION_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+#include <time.h>
+
+double discount[3] = { 0.9,0.85,0.75 };//不同卡对应的折扣
+enum { SLIVER, GOLD, PLATINUM }card;//用户会员类型
 
 //菜单结构体
 typedef struct recipe
 {
     char name[100];          //菜品名称
-    int price;               //菜品价格
-    struct recipe* next = NULL;
+    float price;               //菜品价格
     int num;                 //菜品数量
-};
+
+    struct recipe* next;
+}recipe;
 
 typedef struct r_classify
 {
     char name[100];                 //菜品分类名称
-    struct recipe* r_head = NULL;   //菜品链表
-    struct r_classify* next = NULL;
-};
+    int total_number;        //记录菜品的总数量
+
+    struct recipe* r_head;   //菜品链表
+    struct recipe* r_tail;   //菜品链表尾
+
+    struct r_classify* next; //下一个分类
+}r_classify;
 
 //订单结构体
 typedef struct order
 {
     // 订单号
     char order_id[100];
-    
+
+    //订单创建时间
+    struct tm* localTime;
+
     // 商家，配送员，顾客的名字
-    char m_name[100];          
+    char m_name[100];
     char d_name[100];
     char u_name[100];
 
@@ -39,20 +55,22 @@ typedef struct order
     char d_phone[20];
     char u_phone[20];
 
-    //预计到店时间
-    int arrive_time;
-    //预计送达时间
-    int send_time;
-
     // 订单状态
-    enum {WAY, DILLVERING, FINISH} state;
+    enum { WAY, DILLVERING, FINISH } state;
+
+    //预计到店时间
+    struct tm* arrive_time;
+    //预计送达时间
+    struct tm* send_time;
 
     // 订单的菜品
-    struct r_classify* r_head = NULL;
+    struct recipe* r_head;
+    struct recipe* r_tail;
 
-    int price;               
-    struct order* next = NULL;
-};
+    float sum_price;
+
+    struct order* next;
+}order;
 
 // 商家结构体
 typedef struct Merchant
@@ -62,27 +80,32 @@ typedef struct Merchant
     char address[100];
     char phone[20];
 
-    struct order* o_head = NULL;//订单链表
-    struct r_classify* r_head = NULL;//菜品分类链表
+    struct order* o_head;//订单链表
+    struct order* o_tail;//订单链表尾
 
-    struct Merchant *next = NULL;
+    struct r_classify* r_head;//菜品分类链表
+    struct r_classify* r_tail;//菜品分类链表尾
 
-};
+    struct Merchant* next;
+
+}Merchant;
 
 // 外卖员结构体
 typedef struct DeliveryPerson
 {
-    struct order* o_head = NULL;//订单链表
-    
-    
-    enum {FREE, BUSY} state;//状态
+    struct order* o_head;//订单链表
+
+
+    enum { FREE, BUSY } state;//状态
+
+    char password[100];
     char name[100];
     char phone[20];
 
 
-    struct DeliveryPerson *next = NULL;
-    
-};
+    struct DeliveryPerson* next;
+
+}DeliveryPerson;
 
 // 用户结构体
 typedef struct User
@@ -91,12 +114,76 @@ typedef struct User
     char name[100];
     char address[100];
     char phone[20];
+    
+    int card_class;//用户会员
+    int card_class;//用户会员
 
     //订单号
-    struct order* head = NULL;
+    struct order* head;
 
-    struct User *next = NULL;
-    
-};
+    struct User* next;
+
+}User;
+
+//创建链表
+
+//创建订单菜品列表
+recipe* creatList_recipe_1(char* n, float p, int num);
+//创建菜单里的菜品列表
+recipe* creatList_recipe_2(char* n, float p);
+//创建菜品类列表
+r_classify* creatList_r_classify(char* n);
+//创建订单列表
+order* creatList_order(char* merchant_name, char* merchant_address, char* merchant_phone,
+    char* deliver_name, char* deliver_phone, char* user_name, char* user_adderss, char* user_phone);
+//创建商家链表
+Merchant* creatList_merchant(char* n, char* p, char* a, char* pn);
+//创建用户链表
+User* creatList_user(char* n, char* p, char* a, char* pn);
+// 创建外卖员列表
+DeliveryPerson* creatList_deliveryperson(char* n, char* p, char* pn);
+
+//打印链表
+
+//打印菜单中的菜品
+void printList_recipe_1(recipe* head);
+//打印订单中菜品
+void printList_recipe_2(recipe* head);
+//打印菜品分类
+void printList_r_classify(r_classify* head);
+// 打印订单
+void print_order_list(order* head);
+//打印商家列表
+void printList_merchant(Merchant* head);
+
+//求单个订单的折后价
+void sumPrice(order* head, recipe* head_r, enum card c, int* d);
+
+
+bool delete_order(order** head, char* order_id);
+
+//设置时间
+void set_time(struct tm* localTime);
+
+//修改密码
+
+//更改商户密码
+void merchant_change_p(Merchant* head);
+//更改用户密码
+void user_change_p(User* head);
+//更改外卖员密码
+void deliveryPerson_change_p(DeliveryPerson* head);
+
+//更改会员折扣
+void discount_change(int* d, enum card c);
+//更改用户会员类型
+void card_class_change(User* head, int n);
+
+// 登录以及修改密码
+bool compare(char* n);
+void writeIntoFile_p(char * f_name ,char* n, char* p);
+void updatePassword(char * f_name ,char* n, char* new_p);
+
+
 
 #endif
