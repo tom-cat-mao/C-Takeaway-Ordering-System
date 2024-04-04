e63009db7db6c32fd1e009657973ef71fa9b0c29
Tom Cat <maoy896@gmail.com>
2024-04-04 13:30:22 +0800

    Add new files and directories for CMake build

---
+++ build/CMakeFiles/printList.c.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/d/Cproject/C-/createList.c" "CMakeFiles/printList.c.dir/createList.c.o" "gcc" "CMakeFiles/printList.c.dir/createList.c.o.d"
+  "/mnt/d/Cproject/C-/createTree.c" "CMakeFiles/printList.c.dir/createTree.c.o" "gcc" "CMakeFiles/printList.c.dir/createTree.c.o.d"
+  "/mnt/d/Cproject/C-/deleteTree.c" "CMakeFiles/printList.c.dir/deleteTree.c.o" "gcc" "CMakeFiles/printList.c.dir/deleteTree.c.o.d"
+  "/mnt/d/Cproject/C-/delete_data.c" "CMakeFiles/printList.c.dir/delete_data.c.o" "gcc" "CMakeFiles/printList.c.dir/delete_data.c.o.d"
+  "/mnt/d/Cproject/C-/discount_card_change.c" "CMakeFiles/printList.c.dir/discount_card_change.c.o" "gcc" "CMakeFiles/printList.c.dir/discount_card_change.c.o.d"
+  "/mnt/d/Cproject/C-/insertTree.c" "CMakeFiles/printList.c.dir/insertTree.c.o" "gcc" "CMakeFiles/printList.c.dir/insertTree.c.o.d"
+  "/mnt/d/Cproject/C-/searchTree.c" "CMakeFiles/printList.c.dir/searchTree.c.o" "gcc" "CMakeFiles/printList.c.dir/searchTree.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/printList.c.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/printList.c.dir/createList.c.o"
+  "CMakeFiles/printList.c.dir/createList.c.o.d"
+  "CMakeFiles/printList.c.dir/createTree.c.o"
+  "CMakeFiles/printList.c.dir/createTree.c.o.d"
+  "CMakeFiles/printList.c.dir/deleteTree.c.o"
+  "CMakeFiles/printList.c.dir/deleteTree.c.o.d"
+  "CMakeFiles/printList.c.dir/delete_data.c.o"
+  "CMakeFiles/printList.c.dir/delete_data.c.o.d"
+  "CMakeFiles/printList.c.dir/discount_card_change.c.o"
+  "CMakeFiles/printList.c.dir/discount_card_change.c.o.d"
+  "CMakeFiles/printList.c.dir/insertTree.c.o"
+  "CMakeFiles/printList.c.dir/insertTree.c.o.d"
+  "CMakeFiles/printList.c.dir/searchTree.c.o"
+  "CMakeFiles/printList.c.dir/searchTree.c.o.d"
+  "printList.c"
+  "printList.c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/printList.c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
