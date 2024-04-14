6dd97d1d318a807433f34c69c429afc0ec21b0eb
Tom Cat <maoy896@gmail.com>
2024-04-14 11:20:38 +0800

    Add file system functionality and update CMakeLists.txt

--- build/CMakeFiles/MyProject.dir/cmake_clean.cmake
+++ build/CMakeFiles/MyProject.dir/cmake_clean.cmake
@@ -9,6 +9,8 @@ file(REMOVE_RECURSE
   "CMakeFiles/MyProject.dir/delete_data.c.o.d"
   "CMakeFiles/MyProject.dir/discount_card_change.c.o"
   "CMakeFiles/MyProject.dir/discount_card_change.c.o.d"
+  "CMakeFiles/MyProject.dir/folder_c_c_d.c.o"
+  "CMakeFiles/MyProject.dir/folder_c_c_d.c.o.d"
   "CMakeFiles/MyProject.dir/insertTree.c.o"
   "CMakeFiles/MyProject.dir/insertTree.c.o.d"
   "CMakeFiles/MyProject.dir/name_a_pass.c.o"
@@ -19,6 +21,8 @@ file(REMOVE_RECURSE
   "CMakeFiles/MyProject.dir/searchTree.c.o.d"
   "CMakeFiles/MyProject.dir/sum_price.c.o"
   "CMakeFiles/MyProject.dir/sum_price.c.o.d"
+  "CMakeFiles/MyProject.dir/time_set.c.o"
+  "CMakeFiles/MyProject.dir/time_set.c.o.d"
   "MyProject"
   "MyProject.pdb"
 )
