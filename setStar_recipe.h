44357685644b3ce03877b501b626fc07c64eeec5
stephenzhang0529 <143581044+stephenzhang0529@users.noreply.github.com>
2024-04-14 09:01:29 +0800

    improve setStar_recipe.h

--- setStar_recipe.h
+++ setStar_recipe.h
@@ -2,14 +2,14 @@
 #ifndef _SETSTAR_RECIPE
 #define _SETSTAR_RECIPE
 #include "class.h"
-int generateRandomNumber()
+int generateRandomStar()
 {
     // set a seed
     srand(time(NULL));
 
     // generate a random int from one to five
-    int randomNumber = rand() % 5+1;
+    int randomStar = rand() % 5+1;
 
-    return randomNumber;
+    return randomStar;
 }
 #endif
