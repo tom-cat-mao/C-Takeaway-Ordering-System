b841902d958359df295334a08af4e4a52d622273
stephenzhang0529 <143581044+stephenzhang0529@users.noreply.github.com>
2024-04-17 19:24:14 +0800

    renew wordcolour and interface
    add the menu of manager and improve the wordcolour.h

--- wordcolour.h
+++ wordcolour.h
@@ -1,26 +1,36 @@
 #pragma once
 #ifndef _WORDCOLOUR_H
 #define _WORDCOLOUR_H
-#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
-#define COLOR_FORE_RED      "\x1b[31m"//set the foreground color to red
-#define COLOR_FORE_GREEN    "\x1b[32m"//set the foreground color to green
-#define COLOR_FORE_BACK_DEFAULT   "\x1b[39m"//set both ground colors to defluat
-#define COLOR_BACK_DEFLAUT  "\x1b[49m"//set the background color to defluat
+#include <stdio.h>
 #include <windows.h>
-#define CONFIG_CONSOLE_COLOR \
-HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE); \
-if (hOut == INVALID_HANDLE_VALUE) \
-{ \
-    return GetLastError(); \
-} \
-DWORD dwMode = 0; \
-if (!GetConsoleMode(hOut, &dwMode)) \
-{ \
-    return GetLastError(); \
-} \
-dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING; \
-if (!SetConsoleMode(hOut, dwMode)) \
-{ \
-    return GetLastError(); \
+/*
+the corresponding colour of word_colour
+0 = black
+1 = blue
+3 = light green
+4 = red
+5 = purple
+6 = yellow
+7 = white
+8 = gray
+9 = light blue
+10 = light green
+11 = light light green
+12 = light red
+13 = light purple
+14 = light yellow
+15 = bright white
+*/
+void colour(const int word_colour)
+{
+    if (word_colour >= 0 && word_colour <= 15)
+    {
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), word_colour);
+    }
+    else//set the word colour to default
+    {
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+    }
+
 }
 #endif
