9c6c4365b7e8f329befae3d78848ca75cb7d8ae3
Tom_Cat <maoy896@gmail.com>
2024-04-26 21:29:23 +0800

    add delete_folder.c and delete_folder.h

---
+++ delete_folder.h
@@ -0,0 +1,9 @@
+#ifndef MYPROJECT_DELETE_FOLDER_H
+#define MYPROJECT_DELETE_FOLDER_H
+#include <stdbool.h>
+
+bool delete_m_u_d_folder ();
+
+bool  delete_folder(const char *folderPath);
+
+#endif //MYPROJECT_DELETE_FOLDER_H
